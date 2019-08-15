c25236f14495e8406552157605cf6f3c9c36b85f
League Student <league@iMac-18.attlocal.net>
2019-08-15 14:27:36 -0700

    Day 4!

--- src/section4/RobotTreasureHunt.java
+++ src/section4/RobotTreasureHunt.java
@@ -15,44 +15,53 @@ import org.jointheleague.graphical.robot.Robot;
 
 public class RobotTreasureHunt implements KeyEventDispatcher{
 
-	// 1. Create a new mini robot (type "mini" inside the parentheses)
-	
+	private String mini;
+// 1. Create a new mini robot (type "mini" inside the parentheses)
+Robot zane=new Robot(mini);
 	private void goUp() throws InterruptedException {
 		// 2. Make the robot move up the screen (use setAngle(angle) and microMove(distance))
-		
+		zane.setAngle(0);
+		zane.microMove(500);
 	}
 
 	private void goDown() throws InterruptedException{
 		// 3. make the robot move down the screen (use setAngle(angle) and microMove(distance))
-		
+		zane.setAngle(180);
+		zane.microMove(500);
 	}
 
 	private void turnLeft() throws InterruptedException{
 		// 4. Make the robot turn to the left (use setAngle(angle) and microMove(distance))
-
+		zane.setAngle(-90);
+		zane.microMove(500);
 	}
 
 	private void turnRight() throws InterruptedException{
 		// 5. make the robot turn to the right (use setAngle(angle) and microMove(distance))
-		
+		zane.setAngle(90);
+		zane.microMove(500);
 	}
 
 	private void spaceBarWasPressed() {
 
 		// 5. Change ROBOTNAME below to match the name of the robot you created in step 1.  THEN, remove the slashes at the beginning of the next two lines
-		//int robotXLocation = ROBOTNAME.getX();
-		//int robotYLocation = ROBOTNAME.getY();
+		int robotXLocation = zane.getX();
+		int robotYLocation = zane.getY();
 		
 		// 6. Print the robotXLocation and robotYLocation variables to the console 
-		
+		System.out.println(robotXLocation);
+		System.out.println(robotYLocation);
 		// 7. If robot is at same location as the little girl
+		if (robotXLocation==400) {
+		if (robotYLocation==400) {
 		//      --make a pop-up tell the robot where to go next
-		
+			JOptionPane.showMessageDialog(null,"hi");}
 		// 8. Give the user subsequent clues at different locations on the image
 		// (pirate robot, swamp, parrots, etc.)
 		
 		// 9.  If the robot is in the final location
 		//     --call the treasureFound() method
+		}}
 		
 	}
 
